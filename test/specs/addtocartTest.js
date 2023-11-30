1735e325f78a5ff6129bf62276541d82de8a3417
ezrachecketts <ezra.checketts6509@stu.mtec.edu>
2023-11-30 11:35:00 -0700

    Created addtocartTest.js

---
+++ test/specs/addtocartTest.js
@@ -0,0 +1,13 @@
+import { expect } from '@wdio/globals'
+import LoginPage from '../pageobjects/loginPage.js'
+import InventoryPage from '../pageobjects/inventoryPage.js'
+
+describe('The website', () => {
+    it('should login properly and add an item to the cart', async () => {
+        await LoginPage.open();
+        await LoginPage.login('standard_user', 'secret_sauce');
+        await expect(InventoryPage.appLogo).toExist();
+        await InventoryPage.clickElement(InventoryPage.addtocartBackpack);
+        await expect(InventoryPage.shoppingcartBadge).toExist();
+    })
+})
